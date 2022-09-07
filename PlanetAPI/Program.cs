bd73e6ac2c3402c4df5e0e313efb8f0974ccd81c
dylanmccarthy <dylan.v.mccarthy@gmail.com>
2022-09-07 10:43:54 +1000

    this version is working in rider, need to try and remove mysql password from compose into env file

--- PlanetAPI/Program.cs
+++ PlanetAPI/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<PlanetDbContext>(options =>
 {
-options.UseMySQL(Configuration["DB:PlanetConnection"]);
+options.UseMySQL(Configuration["DB_PlanetConnection"]);
 });
 
 var app = builder.Build();
